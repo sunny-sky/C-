cb38ac02e1412c8b8febc40cd7e2d345fd5ff443
sunny-sky <422385795@qq.com>
2019-03-14 23:50:19 +0800

    init

---
+++ No_026/源1.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxArea(vector<int>& height) {
+		int water = 0;
+		int i = 0, j = height.size() - 1;
+		while (i < j)
+		{
+
+		}
+	}
+};
